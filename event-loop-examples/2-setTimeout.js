324416b1c5834fe4172fbdbd1d1f0a7de4a93b4c
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-11-12 10:30:25 +0800

    rearrange file

--- event-loop-examples/2-setTimeout.js
+++
@@ -1,5 +0,0 @@
-console.log("first");
-setTimeout(() => {
-  console.log("second");
-}, 0); // even the time given is 0 still the output will be first third second because the setTimeout is a asynchronus function.
-console.log("third");
--- event-loop-examples/3-setInterval.js
+++
@@ -1,6 +0,0 @@
-console.log("first tasks");
-setInterval(() => {
-  console.log("hello world");
-}, 2000);
-
-console.log("next task");
